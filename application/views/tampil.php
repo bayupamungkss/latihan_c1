125a10cac7990b96161d4af2b264bc096e2e42e3
bayupamungkss <bayupamungkss.kuliah@gmail.com>
2022-09-20 16:11:53 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Latihan Pertama</title>
+</head>
+<body>
+	<p>Latihan Pertama saya belajar framework CodeIgniter</p>
+	<p>framework CodeIgniter sangat mudah dipelajari</p>
+	
+</body>
+</html>
+
